4d0d308232a606856619e14664e783ebbc0d2a0e
2022148023 <m0o01@yonsei.ac.kr>
2023-05-27 23:59:15 +0900

    landing page

--- index.js
+++ index.js
@@ -16,12 +16,16 @@ app.engine("html", require("ejs").renderFile);
 app.set("views", path.join(__dirname, "views"));
 
 // main page
-app.get("/", function (req, res) {
-  res.render("home", {
+app.get("/map", function (req, res) {
+  res.render("map", {
     KAKAO_MAP_JAVASCRIPT_KEY:
       process.env.KAKAO_MAP_JAVASCRIPT_KEY ||
       "64e36d7df07a48189e25336dc4137d96",
   });
+});
+
+app.get("/", function (req, res) {
+  res.render("home");
 });
 
 app.get("/camera", (req, res) => {
