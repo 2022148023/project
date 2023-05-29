db4fb4b0f7bad260146e226d20dbd2012cafce0c
2022148023 <m0o01@yonsei.ac.kr>
2023-05-29 22:42:10 +0900

    connect ai model

--- public/scripts/scan.js
+++ public/scripts/scan.js
@@ -67,13 +67,29 @@ const handleStream = (stream) => {
   streamStarted = true;
 };
 
-const doScreenshot = () => {
+const doScreenshot = async () => {
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
   canvas.getContext("2d").drawImage(video, 0, 0);
   screenshotImage.src = canvas.toDataURL("image/webp");
   screenshotContainer.style.display = "block";
   screenshotImage.classList.remove("d-none");
+  if (!model) {
+    await initializeTeachableMachineModel();
+  }
+  setTimeout(async () => {
+    const prediction = await model.predict(canvas);
+    prediction.sort(
+      (prediction1, prediction2) =>
+        prediction2.probability - prediction1.probability
+    );
+    console.log(prediction);
+    Toast.fire({
+      icon: "info",
+      title: `Our AI model thinks that this is ${prediction[0].className}`,
+    });
+    screenshotContainer.style.display = "none";
+  }, 2000);
 };
 
 screenshotButton.onclick = doScreenshot;
@@ -88,3 +104,58 @@ document.addEventListener("DOMContentLoaded", async () => {
     });
   }
 });
+
+// More API functions here:
+// https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
+
+// the link to your model provided by Teachable Machine export panel
+const teachableMachineModelURL =
+  "https://teachablemachine.withgoogle.com/models/XZQGv8mFR/";
+
+let model, maxPredictions;
+
+// Load the image model and setup the webcam
+async function initializeTeachableMachineModel() {
+  const modelURL = teachableMachineModelURL + "model.json";
+  const metadataURL = teachableMachineModelURL + "metadata.json";
+
+  // load the model and metadata
+  // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+  // or files from your local hard drive
+  // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+  model = await tmImage.load(modelURL, metadataURL);
+  maxPredictions = model.getTotalClasses();
+
+  /*   // Convenience function to setup a webcam
+  const flip = true; // whether to flip the webcam
+  webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
+  await webcam.setup(); // request access to the webcam
+  await webcam.play();
+  window.requestAnimationFrame(loop); */
+
+  /*   // append elements to the DOM
+  document.getElementById("webcam-container").appendChild(webcam.canvas);
+  labelContainer = document.getElementById("label-container");
+  for (let i = 0; i < maxPredictions; i++) {
+    // and class labels
+    labelContainer.appendChild(document.createElement("div"));
+  } */
+}
+
+/* async function loop() {
+  webcam.update(); // update the webcam frame
+  await predict();
+  window.requestAnimationFrame(loop);
+} */
+
+// run the webcam image through the image model
+/* async function predict() {
+  // predict can take in an image, video or canvas html element
+  const prediction = await model.predict(webcam.canvas);
+  for (let i = 0; i < maxPredictions; i++) {
+    const classPrediction =
+      prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+    labelContainer.childNodes[i].innerHTML = classPrediction;
+  }
+}
+ */
